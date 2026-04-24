875d6ff1dee68578474799fd8988ad337fc299a9
Inna Saienko <inna.saienko.nl@gmail.com>
2026-04-24 11:16:31 +0200

    feat: create login form control

--- src/js/task-4.js
+++ src/js/task-4.js
@@ -1,1 +1,26 @@
+const form = document.querySelector('.login-form');
 
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const {email, password} = form.elements;
+
+    const emailTrimmed = cleanValue(email.value);
+    const passwordTrimmed = cleanValue(password.value);
+
+    if ( emailTrimmed === '' || passwordTrimmed === '' ) {
+        alert('All form fields must be filled in');
+        return
+    }
+    const formData = {
+        email: emailTrimmed,
+        password: passwordTrimmed,
+    };
+    console.log("Form data", formData);
+    form.reset();
+
+})
+
+function cleanValue(value) {
+      return value.trim()
+}
